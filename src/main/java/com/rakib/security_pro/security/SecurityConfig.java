494e9df94455efaf68be412569a2b584647e264c
akter_rakib <rakibccj@gmail.com>
2020-08-04 16:10:25 +0600

    customization authentication manager    apply in memory

---
+++ src/main/java/com/rakib/security_pro/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.rakib.security_pro.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("rakib")
+                .password("rakib")
+                .roles("USER");
+    }
+}
